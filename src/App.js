f4ce416dda1e1196816fee42aacc3c424d4996e0
db <talktodonnybronson@gmail.com>
2019-11-13 18:27:06 +0000

    first test

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React,{ Component }  from 'react'
+
+import QrReader from 'react-qr-reader';
+
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header Container">
+        <Test/>
+
+      </header>
+    </div>
+  );
+}
+class Test extends Component {
+  state = {
+    result: 'No result'
+  }
+
+  handleScan = data => {
+    if (data) {
+      this.setState({
+        result: data
+      })
+    }
+  }
+  handleError = err => {
+    console.error(err)
+  }
+  render() {
+    return (
+      <div>
+        <QrReader
+          delay={300}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          style={{ width: '100%' }}
+        />
+        <div className="header-copy">Below is the output from the scanner</div>
+        <p>{this.state.result}</p>
+        <Copy/>
+      </div>
+    )
+  }
+}
+class Copy extends Component {
+  state ={
+
+  }
+  render(){
+    return (
+      <div className="box-copy"> Here we can put some copy about the globes etc
+      </div>
+
+    )
+  }
+
+}
+
+
+
+export default App;
